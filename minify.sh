4f571cfb518c814beba7d3f33f9777d08e90ebac
salah ahmed <salah.ahmed.hslc@gmail.com>
2018-06-15 06:26:40 -0400

    minify html

--- minify.sh
+++ minify.sh
@@ -3,4 +3,8 @@ for full_path in $dir; do
     file=`basename $full_path` 
     uglifyjs --compress --mangle -- $full_path > static/js/${file%.js}.min.js
 done
-
+dir="src/html/*"
+for full_path in $dir; do
+    file=`basename $full_path` 
+    html-minifier --collapse-whitespace --minify-js 1 $full_path > templates/$file
+done
