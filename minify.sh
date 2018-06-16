1d35b882112a71fed889eea7af4a3e4ded767995
salah ahmed <salah93@crosscompute.com>
2018-06-16 15:02:03 -0400

    make directories in case they don't exist

--- minify.sh
+++ minify.sh
@@ -1,9 +1,11 @@
+mkdir -p static/js
 dir="src/js/*"
 for full_path in $dir; do
     file=`basename $full_path` 
     uglifyjs --compress --mangle -- $full_path > static/js/${file%.js}.min.js
 done
 dir="src/html/*"
+mkdir -p templates
 for full_path in $dir; do
     file=`basename $full_path` 
     html-minifier --collapse-whitespace --minify-js 1 $full_path > templates/${file%.html}.min.html
